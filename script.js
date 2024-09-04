625190aa272efd2d59b5241b2c85b17fc489e4ff
markochen <chenmarko77@gmail.com>
2024-09-04 21:45:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,100 @@
+// // Парсить з рядка ціле число
+// console.log(Number.parseInt('5px')); // 5
+// console.log(Number.parseInt('12qwe74')); // 12
+// console.log(Number.parseInt('12.46qwe79')); // 12
+// console.log(Number.parseInt('qweqwe')); // NaN
+
+
+
+
+// // Парсить з рядка дробове число
+// console.log(Number.parseFloat('5px')); // 5
+// console.log(Number.parseFloat('12qwe74')); // 12
+// console.log(Number.parseFloat('12.46qwe79')); // 12.46
+// console.log(Number.parseFloat('qweqwe')); // NaN 
+
+// let a = "a"
+// const b = "1"
+// let a1 = a + b 
+// console.log(a1);
+
+// let a2 = a+2
+// console.log(a2);
+
+// let a2 = a+2
+// console.log(a2);
+
+// let a1 = 5+3
+// console.log(a1);
+
+// let a2 = 5-3
+// console.log(a2);
+
+// let a3 = 5*3
+// console.log(a3);
+
+// let a4 = 5/3
+// console.log(a4);
+
+
+
+// let a6 = 5%3
+// alert(a6);
+
+// let a7 = 3%5
+// alert(a7);
+
+// let a8 = 5+'3'
+// alert(a8);
+
+// let a9 = '5'-3
+// alert(a9);
+
+// let a10 = 75+'kg'
+// alert(a10);
+
+
+// console.log(Math.random()*6);
+
+// console.log(Math.round(Math.random()*6));
+
+
+// const x = Number(prompt('enter number'))
+// const y = Number(prompt('enter second number'))
+
+// let z=x+y
+// alert(z)
+
+
+
+let k1 = parseInt('5px');
+console.log(k1);
+
+let k2 = parseInt('12djd334px');
+console.log(k2);
+
+let k3 = parseInt('12.45asdwe12');
+console.log(k3);
+
+let k4 = parseInt('qwqweeewq');
+console.log(k4);
+
+
+let k5 = parseFloat('5px');
+console.log(k5);
+
+let k6 = parseFloat('12djd334px');
+console.log(k6);
+
+let k7 = parseFloat('12.45asdwe12');
+console.log(k7);
+
+let k8 = parseFloat('qwqweeewq');
+console.log(k8);
+
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+
+
+
+console.log(Math.round(Math.random(3) * (19 - 3) + 3));
